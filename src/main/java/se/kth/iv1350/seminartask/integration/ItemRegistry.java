95d0d99dcb5649545bb5dec826e53811ad343dde
jontetk <jonkar2@kth.se>
2023-04-26 17:15:42 +0200

    changed item reg

--- src/main/java/se/kth/iv1350/seminartask/integration/ItemRegistry.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/ItemRegistry.java
@@ -4,7 +4,9 @@ public class ItemRegistry {
 
     private ItemDTO[] items = {new ItemDTO(1,"Milk",(new Cash(15,"I$")),0.34),
     new ItemDTO(2,"Iphone",new Cash(1500000,"I$"),0.01),
-    new ItemDTO(2,"Iphone",new Cash(1500000,"I$"),0.01)}
+    new ItemDTO(3,"Pear",new Cash(150000000,"I$"),10)},
+    new ItemDTO(4,"Cookie",new Cash(25,"I$"),0.21),
+    new ItemDTO(5,"Candy",new Cash(100,"I$"),0.1);
 
     ItemRegistry () {
 
