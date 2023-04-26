e580a9c9da5a93a628dde36975fe73211b3b5f4e
jontetk <jonkar2@kth.se>
2023-04-26 16:50:26 +0200

    added itemDTO class

--- src/main/java/se/kth/iv1350/seminartask/util/ItemDTO.java
+++ src/main/java/se/kth/iv1350/seminartask/util/ItemDTO.java
@@ -3,8 +3,28 @@ package se.kth.iv1350.seminartask.util;
 public class ItemDTO {
     private int ItemID;
     private String description;
-    private 
+    private Cash price;
+    private float VatRate;
+
+
     public ItemDTO(int ItemID, String description, Cash price,float vatRate) {
+        this.ItemID = ItemID;
+        this.description = description;
+        this.price = price;
+        this.VatRate = vatRate;
+    }
+    public int getItemID() {
+        return this.ItemID;
 
     }
+    public String getDescription() {
+        return this.description;
+    }
+    public Cash getPrice() {
+        return this.price;
+    }
+    public float getVatRate() {
+        return this.VatRate; 
+    }
+    
 }
