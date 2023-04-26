cc3d06ebb386b1ea8741e6b39edbc38017d483a2
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-04-26 17:41:00 +0200

    asdas

--- src/test/java/se/kth/iv1350/seminartask/integration/ItemRegistryTest.java
+++ src/test/java/se/kth/iv1350/seminartask/integration/ItemRegistryTest.java
@@ -9,9 +9,23 @@ public class ItemRegistryTest {
 
     @Test
     void testSearchItemWithIdOfOne() {
-        int expecteditem = 1;
-        assertEquals(expecteditem, itemReg.searchItem(1).getItemID());
+        int expectedId = 1;
+        assertEquals(expectedId, itemReg.searchItem(1).getItemID());
         
 
     }
+
+    @Test
+    void testSearchItemWithIdofFive() {
+        int expectedId =5;
+        assertEquals(expectedId, itemReg.searchItem(5).getItemID());
+    }
+
+    @Test
+    void testSearchItemWithInvalidId() {
+        assertNull(itemReg.searchItem(-1), "Item with Inalid Id was fetched ");
+    }
+
+    
+
 }
