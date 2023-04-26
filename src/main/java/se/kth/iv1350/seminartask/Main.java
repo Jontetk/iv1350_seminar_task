9fb8a96abe6a2484fc98e9a36e9a5f43b0c6d33b
jontetk <jonkar2@kth.se>
2023-04-26 15:47:18 +0200

    test

---
+++ src/main/java/se/kth/iv1350/seminartask/Main.java
@@ -0,0 +1,9 @@
+package se.kth.iv1350.seminartask;
+
+public class Main {
+    
+
+    public static int main(String[] args) {
+        return 0;
+}
+}
---
+++ src/test/java/se/kth/iv1350/seminartask/MainTest.java
@@ -0,0 +1,13 @@
+package se.kth.iv1350.seminartask;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+
+public class MainTest {
+    @Test
+    void testMain() {
+        fail("Test the failing");
+    }
+}
