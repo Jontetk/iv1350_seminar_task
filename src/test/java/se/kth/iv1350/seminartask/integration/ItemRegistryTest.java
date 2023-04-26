72950548f3fefa667cfdf76d0353b4f0c5dff110
jontetk <jonkar2@kth.se>
2023-04-26 17:33:11 +0200

    added test

--- src/test/java/se/kth/iv1350/seminartask/integration/ItemRegistryTest.java
+++ src/test/java/se/kth/iv1350/seminartask/integration/ItemRegistryTest.java
@@ -1,16 +1,16 @@
 package se.kth.iv1350.seminartask.integration;
-
+import se.kth.iv1350.seminartask.util.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
+
 
 public class ItemRegistryTest {
     ItemRegistry itemReg = new ItemRegistry();
 
     @Test
     void testSearchItemWithIdOfOne() {
-        int expectedId =1 ;
-        assertEquals(1, itemReg.searchItem(1).getItemID());
+        int expecteditem = 1;
+        assertEquals(expecteditem, itemReg.searchItem(1).getItemID());
         
 
     }
