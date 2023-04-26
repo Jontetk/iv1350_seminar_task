6f0b9bc08278329a7d4a50996477b6accd724070
jontetk <jonkar2@kth.se>
2023-04-26 17:09:04 +0200

    hello :)

--- src/main/java/se/kth/iv1350/seminartask/integration/ItemRegistry.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/ItemRegistry.java
@@ -1,11 +1,13 @@
 package se.kth.iv1350.seminartask.integration;
-
+import se.kth.iv1350.seminartask.util.*;
 public class ItemRegistry {
 
-    private ItemDTO[] items = {};
+    private ItemDTO[] items = {new ItemDTO(1,"Milk",(new Cash(15,"I$")),0.34),
+    new ItemDTO(2,"Iphone",new Cash(1500000,"I$"),0.01),
+    new ItemDTO(2,"Iphone",new Cash(1500000,"I$"),0.01)}
 
-    ItemRegistry (){
-        
+    ItemRegistry () {
+
     }
     public ItemDTO searchItem (int id) {
         for (ItemDTO item : items) {
--- src/main/java/se/kth/iv1350/seminartask/util/ItemDTO.java
+++ src/main/java/se/kth/iv1350/seminartask/util/ItemDTO.java
@@ -4,32 +4,49 @@ public class ItemDTO {
     private int ItemID;
     private String description;
     private Cash price;
-    private float VatRate;
+    private double VatRate;
     /**
      * 
      * @param ItemID The id for the item
      * @param description The description for the item
-     * @param price The price of the item as a Cash object
+     * @param price The price of the item as a <code>Cash</code> object
      * @param vatRate The VatRate of the item
      */
 
-    public ItemDTO(int ItemID, String description, Cash price,float vatRate) {
+    public ItemDTO(int ItemID, String description, Cash price,double vatRate) {
         this.ItemID = ItemID;
         this.description = description;
         this.price = price;
         this.VatRate = vatRate;
     }
+    /**
+     * 
+     * @return The itemID of the <code>itemDTO</code>
+     */
     public int getItemID() {
+        
         return this.ItemID;
 
     }
+    /**
+     * 
+     * @return The description of the <code>itemDTO</code>
+     */
     public String getDescription() {
         return this.description;
     }
+    /**
+     * 
+     * @return The price of the <code>itemDTO</code>
+     */
     public Cash getPrice() {
         return this.price;
     }
-    public float getVatRate() {
+    /**
+     * 
+     * @return The VatRate of the <code>itemDTO</code>
+     */
+    public double getVatRate() {
         return this.VatRate; 
     }
     
