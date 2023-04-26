44df47cd66239a3f1028582a1cb95c7326085257
jontetk <jonkar2@kth.se>
2023-04-26 16:24:00 +0200

    Added test for testing

---
+++ src/test/java/se/kth/iv1350/seminartask/controller/ControllerTest.java
@@ -0,0 +1,4 @@
+package se.kth.iv1350.seminartask.controller;
+
+public class ControllerTest {
+}
---
+++ src/test/java/se/kth/iv1350/seminartask/statup/MainTest.java
@@ -0,0 +1,12 @@
+package se.kth.iv1350.seminartask.statup;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MainTest {
+    @Test
+    void testMain() {
+        fail("testing of fail");
+    }
+}
