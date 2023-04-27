2fc11b20833c68d9053c14b91e1e0b4fdbbc1dfa
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-04-27 15:31:49 +0200

    Printer Class created

---
+++ src/main/java/se/kth/iv1350/seminartask/integration/Printer.java
@@ -0,0 +1,13 @@
+package se.kth.iv1350.seminartask.integration;
+
+public class Printer {
+    
+    public Printer(){
+
+    }
+
+    public void printReceipt(SaleLog currentSaleLog)
+    {
+        /*TODO Implement print receipt using system.out.print?? No need to test it. */
+    }
+}
