5757203cebf57204c124cf8f7c96c91be0a09b54
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-04-27 15:51:08 +0200

    ASdasd

--- src/main/java/se/kth/iv1350/seminartask/controller/Controller.java
+++ src/main/java/se/kth/iv1350/seminartask/controller/Controller.java
@@ -1,5 +1,18 @@
 package se.kth.iv1350.seminartask.controller;
 
+import se.kth.iv1350.seminartask.integration.RegistryCreator;
+
+
 public class Controller {
-    
+    RegistryCreator creator;
+    Printer printer;
+    public Controller(RegistryCreator creator, Printer printer){
+        this.creator = creator;
+        this.printer = printer;
+    }
+
+
+    public RegisteredItems startSale(){
+        /*TODO Implement after implementing RegisteredItems class */
+    }
 }
---
+++ src/main/java/se/kth/iv1350/seminartask/model/RegisteredItems.java
@@ -0,0 +1,24 @@
+package se.kth.iv1350.seminartask.model;
+
+import java.util.ArrayList;
+
+public class RegisteredItems {
+    ArrayList<ScannedItem> items;
+    Cash totalPrice;
+    Cash totalVAT;
+    
+    public ArrayList<ScannedItem> getItems() {
+        return items;
+    }
+    public Cash getTotalPrice() {
+        return totalPrice;
+    }
+    public Cash getTotalVAT() {
+        return totalVAT;
+    }
+
+    public void addItem() {
+        /*TODO Implement addItem */
+    }
+    
+}
