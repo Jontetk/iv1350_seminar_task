badd17bd7e860c9de8f98e1a2c7b66b750a8957e
jontetk <jonkar2@kth.se>
2023-04-27 15:29:16 +0200

    hello

--- src/main/java/se/kth/iv1350/seminartask/integration/AccountingRegistry.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/AccountingRegistry.java
@@ -1,11 +1,21 @@
 package se.kth.iv1350.seminartask.integration;
 
 public class AccountingRegistry {
+	SaleLog currentSaleLog
+
+	/**
+	 * Class that should save the sale log for each sale to an external database
+	 * 
+	 * 
+	 */
     AccountingRegistry() {
 
 	}
-
+	/**
+	 * 
+	 * @param currentSaleLog The <code>SaleLog</code> that should be saved in the <code>AccountRegistry</code>
+	 */
 	public void saveSaleLog(SaleLog currentSaleLog) {
-
+		this.currentSaleLog = currentSaleLog;
 	}
 }
