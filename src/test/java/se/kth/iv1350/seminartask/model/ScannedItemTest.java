fe009aca62cab816137c5afc8f0b07cc275e2614
jontetk <jonkar2@kth.se>
2023-04-27 17:24:44 +0200

    wooo

--- src/test/java/se/kth/iv1350/seminartask/model/ScannedItemTest.java
+++ src/test/java/se/kth/iv1350/seminartask/model/ScannedItemTest.java
@@ -13,12 +13,16 @@ import org.junit.jupiter.params.*;
 public class ScannedItemTest {
     private ScannedItem scannedItemOne;
     private ScannedItem scannedItemTwo;
+    private ScannedItem scannedItemThree;
     @BeforeEach
     void Setup(){
         ItemDTO itemOne = new ItemDTO(1, null, null, 0);
         ItemDTO itemTwo = new ItemDTO(2, null, null, 0);
+        ItemDTO itemThree = new ItemDTO(1, null, null, 0);
         scannedItemOne = new ScannedItem(itemOne, 10);
         scannedItemTwo = new ScannedItem(itemTwo, 5);
+        scannedItemThree = new ScannedItem(itemThree,15);
+
     }
 
     @Test
@@ -29,5 +33,9 @@ public class ScannedItemTest {
     @Test
     void testEqualsDifferentIds(){
         assertFalse(scannedItemOne.equals(scannedItemTwo), "Objects with different ids equal each other");
+    }
+    @Test
+    void testEqualsSameIds(){
+        assertTrue(scannedItemOne.equals(scannedItemThree), "Objects with different ids does not equal each other");
     }
 }
