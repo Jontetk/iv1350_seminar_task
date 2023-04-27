aab4bbe2ee39bb901587c8f244c12fb9971159e3
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-04-27 15:17:39 +0200

    Created COnstructor for the class and a method

--- src/main/java/se/kth/iv1350/seminartask/integration/RegistryCreator.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/RegistryCreator.java
@@ -1,5 +1,28 @@
 package se.kth.iv1350.seminartask.integration;
 
+
 public class RegistryCreator {
+    ItemRegistry registry;
     
+
+   /**
+    * Creates an object instance responsible for registries
+    * All registry objects are initated under instatiation of the class
+    */
+    public RegistryCreator() {
+        this.registry = new ItemRegistry();
+        
+    }
+
+/*TODO MAKE BETTER COMMENT HERE 
+        |
+        V           */
+/**
+ * 
+ * @return item regisry object 
+ */
+public ItemRegistry getRegistry() {
+    return registry;
+}
+        
 }
