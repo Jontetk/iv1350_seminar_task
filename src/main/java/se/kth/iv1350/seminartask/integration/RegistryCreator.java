6531dce74c05726844516c117884794f8e43cfc5
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-04-27 15:26:02 +0200

    Added field Accounting Registry and its getters

--- src/main/java/se/kth/iv1350/seminartask/integration/RegistryCreator.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/RegistryCreator.java
@@ -2,27 +2,33 @@ package se.kth.iv1350.seminartask.integration;
 
 
 public class RegistryCreator {
-    ItemRegistry registry;
-    
+    ItemRegistry itemRegistry;
+    AccountingRegistry accountingRegistry;
 
    /**
     * Creates an object instance responsible for registries
     * All registry objects are initated under instatiation of the class
     */
     public RegistryCreator() {
-        this.registry = new ItemRegistry();
-        
+        this.itemRegistry = new ItemRegistry();
+        this.accountingRegistry = new AccountingRegistry();
     }
 
-/*TODO MAKE BETTER COMMENT HERE 
-        |
-        V           */
+
 /**
  * 
- * @return item regisry object 
+ * @return an item registry object 
  */
 public ItemRegistry getRegistry() {
-    return registry;
+    return itemRegistry;
+}
+
+/**
+ * 
+ * @return an account registry object
+ */
+public AccountingRegistry getAccountingRegistry() {
+    return accountingRegistry;
 }
         
 }
