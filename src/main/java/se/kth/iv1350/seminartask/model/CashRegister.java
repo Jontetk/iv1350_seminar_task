569ca0d2315e9857b4d27bf03d722bbed66f5751
jontetk <jonkar2@kth.se>
2023-04-29 15:38:15 +0200

    wass

--- src/main/java/se/kth/iv1350/seminartask/model/CashRegister.java
+++ src/main/java/se/kth/iv1350/seminartask/model/CashRegister.java
@@ -5,41 +5,43 @@ import se.kth.iv1350.seminartask.util.*;
 
 public class CashRegister {
     
-    private Cash cashInRegister;
+private Cash cashInRegister;
     
 
 
-/**
-     * Keeps track of the cash in the register and calculates the change
-     * @param cashInRegister The cash that should be in the register from the start
-     */
-	public CashRegister(Cash cashInRegister) {
+    /**
+    * Keeps track of the cash in the register and calculates the change
+    * @param cashInRegister The cash that should be in the register from the start
+    */
+    public CashRegister(Cash cashInRegister) {
         this.cashInRegister = cashInRegister;
 	
-	}
+    }
 
-        // TODO Handle negative Values
-        /**
-         * Adds the paid amount to the <code>CashRegister</code> and returns the change that should be given to the customer
-        * @param paidAmount The amount the customer paid
-        * @param totalPrice The price of the total sale
-        * @return The change that the customer should get back. 
-        */
-	public Cash addPayment(Cash paidAmount,Cash totalPrice) { 
-                if (paidAmount.getCurrency() != "I$"){
-                        return null;
-                }
+
+    /**
+    * Adds the paid amount to the <code>CashRegister</code> and returns the change that should be given to the customer
+    * @param paidAmount The amount the customer paid
+    * @param totalPrice The price of the total sale
+    * @return The change that the customer should get back. 
+    */
+    public Cash addPayment(Cash paidAmount,Cash totalPrice) { 
+        if (paidAmount.getCurrency() != "I$"){
+        return null;
+    }
         double currentInReg = this.cashInRegister.getAmount();
         this.cashInRegister.saveAmount(currentInReg+totalPrice.getAmount());
-
-        return new Cash(paidAmount.getAmount()-totalPrice.getAmount(),"I$");
+        if (paidAmount.getAmount()-totalPrice.getAmount() < 0) 
+            return null;
+        else
+            return new Cash(paidAmount.getAmount()-totalPrice.getAmount(),"I$");
 		
 	}
         /**
-         * 
-         * @return The cash in the register
-         */
+        * 
+        * @return The cash in the register
+        */
         public Cash getCashInRegister() {
-                return cashInRegister;
+            return cashInRegister;
         }
 }
