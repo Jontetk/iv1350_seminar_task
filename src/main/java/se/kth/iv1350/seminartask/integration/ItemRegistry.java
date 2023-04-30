9c9925a1f5163904fc886febe44d17a12ac898b2
jontetk <jonkar2@kth.se>
2023-04-30 15:24:40 +0200

    added test item

--- src/main/java/se/kth/iv1350/seminartask/integration/ItemRegistry.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/ItemRegistry.java
@@ -5,11 +5,12 @@ public class ItemRegistry {
     
     // Local attributes to simulate an external database
 
-    private ItemDTO[] items = {new ItemDTO(1,"Milk",(new Cash(15,"I$")),0.34,100),
+    private ItemDTO[] items = {new ItemDTO(1,"Test_Do_Not_Change",(new Cash(1500,"I$")),0.04,1500),
     new ItemDTO(2,"Iphone",new Cash(1500000,"I$"),0.01,15000),
     new ItemDTO(3,"Pear",new Cash(150000000,"I$"),10,60),
     new ItemDTO(4,"Cookie",new Cash(25,"I$"),0.21,25),
-    new ItemDTO(5,"Candy",new Cash(100,"I$"),0.1,99999999)};
+    new ItemDTO(5,"Candy",new Cash(100,"I$"),0.1,99999999),
+    new ItemDTO(6,"Milk",(new Cash(15,"I$")),0.34,100)};
 
     /**
      * Communicates with an external item database
