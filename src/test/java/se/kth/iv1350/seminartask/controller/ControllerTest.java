71401175294def4067989c814d5aa87ec04719da
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-04-30 15:26:51 +0200

    Testing

--- src/test/java/se/kth/iv1350/seminartask/controller/ControllerTest.java
+++ src/test/java/se/kth/iv1350/seminartask/controller/ControllerTest.java
@@ -1,12 +1,21 @@
 package se.kth.iv1350.seminartask.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.nio.channels.SelectableChannel;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import se.kth.iv1350.seminartask.integration.ItemRegistry;
 import se.kth.iv1350.seminartask.integration.RegistryCreator;
+import se.kth.iv1350.seminartask.model.RegisteredItems;
+import se.kth.iv1350.seminartask.model.ScannedItem;
+import se.kth.iv1350.seminartask.util.*;
 
 
 public class ControllerTest {
@@ -14,16 +23,41 @@ public class ControllerTest {
 
     Controller controller = new Controller(registryCreator, null);
      
-       
+    @BeforeEach
+    void setup() {
+        controller.startSale();
+    }    
    
     @ParameterizedTest 
     @ValueSource(ints = {1,2,3,4})
-    void testSelectItem(int id) { 
-        controller.startSale();
+    void testSelectItemValidIds(int id) { 
+        
         int expectedId = id;
         int actualId =  controller.selectItem(id).getItemID();
         assertEquals(expectedId, actualId, "Ids should be the same");
     
 
     }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 7, 10})
+    void testSelectItemInvalidIds(int id) {
+        assertNull(controller.selectItem(id),"invalid item fetched");
+
+    }
+
+    @Test
+    void testAmountPaid() {
+        controller.selectItem(1, 1); //15 dollars per unit
+        controller.selectItem(4)
+
+    }
+
+    @Test
+    void testGetTotal() {
+        
+    }
+
+
 }
