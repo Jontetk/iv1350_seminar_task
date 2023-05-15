681065665f7e3556e4795e9fb030ba6bcf4b3cd2
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-15 17:04:48 +0200

    should be good now

---
+++ src/main/java/se/kth/iv1350/seminartask/integration/ItemRegistryException.java
@@ -0,0 +1,7 @@
+package se.kth.iv1350.seminartask.integration;
+
+public class ItemRegistryException extends RuntimeException{
+    itemRegistryException(String message){
+    super(message);
+    }
+}
