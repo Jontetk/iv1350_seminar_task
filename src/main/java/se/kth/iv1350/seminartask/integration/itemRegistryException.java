698b9d43f0ad25018c5a756ae856d9cb919b9fea
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-15 16:43:45 +0200

    Added constructor methods

--- src/main/java/se/kth/iv1350/seminartask/integration/itemRegistryException.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/itemRegistryException.java
@@ -1,5 +1,14 @@
 package se.kth.iv1350.seminartask.integration;
 
-public class itemRegistryException {
+public class itemRegistryException extends RuntimeException{
     
+    itemRegistryException(){
+    } 
+
+    itemRegistryException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+
 }
+
