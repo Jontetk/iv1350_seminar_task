d409325dc7f49d5c4fe51311001f0b84db2f0ed2
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-15 16:57:31 +0200

    Deleted default constructor

--- src/main/java/se/kth/iv1350/seminartask/integration/itemRegistryException.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/itemRegistryException.java
@@ -2,9 +2,6 @@ package se.kth.iv1350.seminartask.integration;
 
 public class ItemRegistryException extends RuntimeException{
     
-    ItemRegistryException(){
-    } 
-
     ItemRegistryException(String message){
         super(message);
     }
