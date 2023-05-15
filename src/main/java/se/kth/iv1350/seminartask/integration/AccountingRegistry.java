6716e1b4c9315df09bdeaa6ddce50352145efc7a
jontetk <jonkar2@kth.se>
2023-05-15 17:52:17 +0200

    added AccountingRegistryException

--- src/main/java/se/kth/iv1350/seminartask/integration/AccountingRegistry.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/AccountingRegistry.java
@@ -2,6 +2,9 @@ package se.kth.iv1350.seminartask.integration;
 import se.kth.iv1350.seminartask.model.*;
 public class AccountingRegistry {
 	SaleLog currentSaleLog;
+	private boolean dataBaseAvailable = true;
+
+   
 
 	/**
 	 * Saves the sale log for each sale to an external database
@@ -11,12 +14,17 @@ public class AccountingRegistry {
     AccountingRegistry() {
 
 	}
+	public void setAvailability(boolean condition){
+        this.dataBaseAvailable = condition;
+	}
 	/**
 	 * Saves the <code>SaleLog</code> for the current sale to the accounting database
 	 * 
 	 * @param currentSaleLog The <code>SaleLog</code> that should be saved in the <code>AccountRegistry</code>
 	 */
 	public void saveSaleLog(SaleLog currentSaleLog) {
+		if (this.dataBaseAvailable == false) 
+			throw new AccountingRegistryException("Registery Not running");
 		this.currentSaleLog = currentSaleLog;
 	}
 }
---
+++ src/main/java/se/kth/iv1350/seminartask/integration/AccountingRegistryException.java
@@ -0,0 +1,10 @@
+package se.kth.iv1350.seminartask.integration;
+
+public class AccountingRegistryException extends RuntimeException{
+    
+    AccountingRegistryException(String message) {
+        super(message);
+
+    }
+
+}
