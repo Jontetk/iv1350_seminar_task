bf0d9bf019711ca90e37ae29268b380f4b7597fa
jontetk <jonkar2@kth.se>
2023-05-17 11:13:34 +0200

    Added TotalRevenueFileOutput

--- src/main/java/se/kth/iv1350/seminartask/integration/TotalRevenueFileOutput.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/TotalRevenueFileOutput.java
@@ -1,5 +1,28 @@
 package se.kth.iv1350.seminartask.integration;
 
-public class TotalRevenueFileOutput {
+import se.kth.iv1350.seminartask.model.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+
+
+public class TotalRevenueFileOutput implements SaleObserver {
+    PrintWriter revenueStream = null;
+    private double totalIncome;
+    public TotalRevenueFileOutput() throws IOException{
+        totalIncome = 0;
+        revenueStream = new PrintWriter(
+                    new FileWriter("TotalRevenue"), true);
+
+    }
+    public void newSale(SaleLog saleLog) {
+        totalIncome += saleLog.getTotalPrice().getAmount()+saleLog.getTotalVAT().getAmount();
+        
+        revenueStream.println(totalIncome);
+
+    }
     
 }
