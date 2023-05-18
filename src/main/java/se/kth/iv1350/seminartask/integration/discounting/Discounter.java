bc2d091fc84c3d8337e0cf0f1437b1c1eba688e0
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-18 18:19:23 +0200

    stged stuff

---
+++ src/main/java/se/kth/iv1350/seminartask/integration/discounting/Discounter.java
@@ -0,0 +1,6 @@
+package se.kth.iv1350.seminartask.integration.discounting;
+
+public interface Discounter {
+    
+    
+}
---
+++ src/main/java/se/kth/iv1350/seminartask/util/DiscountDTO.java
@@ -0,0 +1,10 @@
+package se.kth.iv1350.seminartask.util;
+
+public final class DiscountDTO {
+    private String type;
+    private ItemDTO discountedItem;
+    private int amountItemsNeededforDiscount;
+
+}
+
+
