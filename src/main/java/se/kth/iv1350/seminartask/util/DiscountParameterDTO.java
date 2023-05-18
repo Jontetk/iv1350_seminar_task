bb7220f444aa6e828dae11c0d53c2431fcd896f5
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-18 19:18:20 +0200

    b

--- src/main/java/se/kth/iv1350/seminartask/util/DiscountParameterDTO.java
+++ src/main/java/se/kth/iv1350/seminartask/util/DiscountParameterDTO.java
@@ -1,20 +1,32 @@
 package se.kth.iv1350.seminartask.util;
 
+import se.kth.iv1350.seminartask.integration.discounting.Discounter;
 import se.kth.iv1350.seminartask.model.ScannedItem;
 
 public final class DiscountParameterDTO {
-    // TODO Put fields for the discount strategy
+ 
     // This class is going to give the necessary 
     // discount reletad arguments for the strategy 
-    /**
-     * 
-     */
+    private Discounter strategy;
+
     private double minNeededTotal; // Can perhaps be cash??
     private ScannedItem itemAndAmountNeeded;
 
-
+   
     private Cash discountAmount; // Fixed amount not procent
 
+    
+
+    public DiscountParameterDTO(Discounter strategy, double minNeededTotal, ScannedItem itemAndAmountNeeded, Cash discountAmount) {
+        this.minNeededTotal = minNeededTotal;
+        this.itemAndAmountNeeded = itemAndAmountNeeded;
+        this.discountAmount = discountAmount;
+    }
+
+    public Discounter getStrategy() {
+        return strategy;
+    }
+    
     public double getMinNeededTotal() {
         return minNeededTotal;
     }
@@ -27,6 +39,7 @@ public final class DiscountParameterDTO {
         return itemAndAmountNeeded;
     }
 
+    
 
 }
 
