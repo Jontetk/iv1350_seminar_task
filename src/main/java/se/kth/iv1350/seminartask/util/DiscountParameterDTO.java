aeca9534d109bddef536a4d12f23fa9f15caf7a5
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-18 18:25:12 +0200

    changes were made

---
+++ src/main/java/se/kth/iv1350/seminartask/util/DiscountParameterDTO.java
@@ -0,0 +1,16 @@
+package se.kth.iv1350.seminartask.util;
+
+public final class DiscountParameterDTO {
+    // TODO Put fields for the discount strategy
+    // This class is going to give the necessary 
+    // discount reletad arguments for the strategy 
+    /**
+     * 
+     */
+    private String type;
+    private ItemDTO discountedItem;
+    private int amountItemsNeededforDiscount;
+
+}
+
+
