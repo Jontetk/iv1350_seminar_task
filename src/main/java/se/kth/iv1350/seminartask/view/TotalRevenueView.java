7cfb463852c9bcd28a1cea3f39b72d588c4133f5
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-19 19:15:24 +0200

    a

--- src/main/java/se/kth/iv1350/seminartask/view/TotalRevenueView.java
+++ src/main/java/se/kth/iv1350/seminartask/view/TotalRevenueView.java
@@ -2,17 +2,21 @@ package se.kth.iv1350.seminartask.view;
 
 import se.kth.iv1350.seminartask.model.*;
 
+     /**
+     * An observer that writes the total income since the program start  to console 
+     */
 public class TotalRevenueView implements SaleObserver{
     private double totalIncome;
 
+   /**
+    * constructor of the object, the total income is 0 wwhen object is created
+    */
     public TotalRevenueView(){
         totalIncome =0;
     }
-
-
     public void newSale(SaleLog log){
         totalIncome += log.getTotalPrice().getAmount() + log.getTotalVAT().getAmount()-log.getTotalDiscount().getAmount();
-    System.out.format("TotalRevenue: %.3f \n" ,totalIncome);
+        System.out.format("TotalRevenue: %.3f \n" ,totalIncome);
     }
     
 }
