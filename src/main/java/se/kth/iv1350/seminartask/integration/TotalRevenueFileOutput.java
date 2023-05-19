4301ab79afbf72f5d58f0a131e6bfee3a38e87a6
jontetk <jonkar2@kth.se>
2023-05-19 19:11:55 +0200

    added comments

--- src/main/java/se/kth/iv1350/seminartask/integration/TotalRevenueFileOutput.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/TotalRevenueFileOutput.java
@@ -13,12 +13,19 @@ public class TotalRevenueFileOutput implements SaleObserver {
     PrintWriter revenueStream = null;
     private double totalIncome;
     private int saleNum = 0;
+    /**
+     * Creator for the totalrevenuefileout
+     * @throws IOException 
+     */
     public TotalRevenueFileOutput() throws IOException{
         totalIncome = 0;
         revenueStream = new PrintWriter(
                     new FileWriter("TotalRevenue"), true);
 
     }
+    /**
+     * observer that prints revenue to file
+     */
     public void newSale(SaleLog saleLog) {
         totalIncome += saleLog.getTotalPrice().getAmount()+saleLog.getTotalVAT().getAmount()-saleLog.getTotalDiscount().getAmount();
         
