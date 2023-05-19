971705fcc51f2fc17fcad75807b2504332d725de
jontetk <jonkar2@kth.se>
2023-05-19 19:05:34 +0200

    fixed

--- src/main/java/se/kth/iv1350/seminartask/integration/discounting/MultipleOfSameItemDiscount.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/discounting/MultipleOfSameItemDiscount.java
@@ -24,11 +24,11 @@ public class MultipleOfSameItemDiscount implements Discounter{
         int discountApplicationCount = saleItem.getAmount()/ parameterDTO.getItemAndAmountNeeded().getAmount();
         
         if(discountApplicationCount > 0){
-        Double discountAmount = parameterDTO.getFixedDiscountAmount().getAmount() * discountApplicationCount;
-        Cash discountInCash = new Cash(discountAmount, "I$");
-        saleItem.setDiscount(discountInCash);
-        saleLog.addTotalDiscount(discountInCash);
-        saleLog.addAppliedDiscount(parameterDTO.getName());
+            Double discountAmount = parameterDTO.getFixedDiscountAmount().getAmount() * discountApplicationCount;
+            Cash discountInCash = new Cash(discountAmount, "I$");
+            saleItem.setDiscount(discountInCash);
+            saleLog.addTotalDiscount(discountInCash);
+            saleLog.addAppliedDiscount(parameterDTO.getName());
         }
 
         }
