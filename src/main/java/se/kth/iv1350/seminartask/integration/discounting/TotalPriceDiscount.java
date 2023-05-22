8f179b04c46605c2aecc033506f166026be12ca3
jontetk <jonkar2@kth.se>
2023-05-22 17:37:44 +0200

    fixed comments

--- src/main/java/se/kth/iv1350/seminartask/integration/discounting/TotalPriceDiscount.java
+++ src/main/java/se/kth/iv1350/seminartask/integration/discounting/TotalPriceDiscount.java
@@ -4,15 +4,14 @@ import se.kth.iv1350.seminartask.model.SaleLog;
 import se.kth.iv1350.seminartask.util.DiscountParameterDTO;
 
 public class TotalPriceDiscount implements Discounter {
-       
+    public TotalPriceDiscount(){
+
+    }
+    
     /***
      * A fixed amount of discount is applied to log if the total price of the sale exceeds 
      * a specifed amount in the parameterDTO. 
      */
-    public TotalPriceDiscount(){
-
-    }
-
     @Override
     public void discount(DiscountParameterDTO parameterDTO, SaleLog saleLog){
         if(saleLog.getTotalPrice().getAmount() >= parameterDTO.getMinNeededTotal()){
