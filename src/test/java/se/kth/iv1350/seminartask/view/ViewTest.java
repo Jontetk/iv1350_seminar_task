44be8fcb0c6339637ed8799f08756a721dc5d6f9
jontetk <jonkar2@kth.se>
2023-05-29 18:46:36 +0200

    finished ViewTest

--- src/test/java/se/kth/iv1350/seminartask/view/ViewTest.java
+++ src/test/java/se/kth/iv1350/seminartask/view/ViewTest.java
@@ -62,7 +62,6 @@ public class ViewTest {
         String expected = "Item id:\nAmount:\nThe current items are:\n\r\nThe last item was invalid please try again\n\r\nWant to keep scanning? [y/n]:\nItem id:\nAmount:\nThe current items are:\n\r\nLast scanned item:\r\nMilk x 4\n\r\nWant to keep scanning? [y/n]:\nItem id:\nAmount:\nThe current items are:\n\r\nMilk x 4\n\r\nLast scanned item:\r\nCookie x 1\n\r\nWant to keep scanning? [y/n]:Enter customerID :\r\n\nTotal price to pay:78.25I$\r\nPayment:\nThat is not enoguh. Total price to pay:78.25I$\r\nPayment:Change:1.75I$\n\r\nTotalRevenue: 78,250 \n\nMilk x 4 60.0I$\r\nUnit price: 15.0I$ VAT Price: 18.0I$\r\n\r\nCookie x 1 25.0I$\r\nUnit price: 25.0I$ VAT Price: 5.25I$\r\n\r\nApplied discounts: 50% off all milk\r\nTotal Discount: 30.0\r\nTotal VAT: 23.25\r\nTotal with VAT and discount: 78.25\r\nChange: 1.75";
         String scanAgainExpected = "Want to scan again? [y/n]";
         String result = outContent.toString();
-        System.setOut(originalSysOut);
         assertTrue(result.contains(expected),"Incorrect printout");
         assertTrue(result.contains(scanAgainExpected),"Incorrect printout");
     }
